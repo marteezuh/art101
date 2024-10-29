9c3ce09e4ebd5d922f33627b7936b1c24c7d0815
Maritza <maanorte@ucsc.edu>
2024-10-29 10:34:25 -0700

    link

---
+++ lab7/js/lab7.js
@@ -0,0 +1,21 @@
+// index.js - Lab 7: Functions
+// Author: Maritza Ortega
+// Date:28 October 2024
+
+// Constants
+
+// Functions
+
+function sortUserName() {
+    // Prompt the user for their name
+    var userName = window.prompt("Maritza Ortega");
+
+    // Convert the string to an array, sort it, and convert it back to a string
+    var sortedName = userName.split('').sort().join('');
+
+    return sortedName;
+}
+
+// Call the function and output the result
+document.writeln("Sorted letters of your name: " + sortUserName());
+
