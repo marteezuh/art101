ad08273b66d840a5c5c0fa3731d633391817f258
Maritza <maanorte@ucsc.edu>
2024-11-18 06:27:11 -0800

    u

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -4,18 +4,18 @@
 
 
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
-$("#problems").append("<button id='button-problems'>Highlight Problems</button>");
-$("#results").append("<button id='button-results'>Show Results</button>");
+$("#problems").append("<button id='button-problems'>Make Special</button>");
+$("#results").append("<button id='button-results'>Make Special</button>");
 
 $("#button-challenge").click(function() {
     $("#challenge").toggleClass("special");
 });
 
 $("#button-problems").click(function() {
-    $("#problems").toggleClass("highlight");
+    $("#problems").toggleClass("special");
 });
 
 $("#button-results").click(function() {
-    $("#results").toggleClass("visible");
+    $("#results").toggleClass("special");
 });
 
