9dac38ecd0da9b0b8cc8025d36cda68ddd2e5a45
Maritza <maanorte@ucsc.edu>
2024-11-18 06:52:44 -0800

    js

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -7,20 +7,15 @@ function generateRandomText() {
     const min = 3;
     const max = 100;
     const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-    // Get a random starting index to slice the Lorem Ipsum text
     const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-    // Generate the random Lorem Ipsum-like text
     return text.slice(randStart, randStart + randLen);
-  }
+}
 
-  // click listener for button
-$("#make-convo").click(function(){
+$("#make-convo").click(function() {
+   
+    const newText = generateRandomText();
+
+
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
 });
 
-
-// get new fake dialogue
-const newText = generateRandomText();
-
-// append a new div to our output div
-$("#output").append('<div class="text"><p>' + newText + '</p></div>');
-  
