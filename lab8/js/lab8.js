5be97244b74d2bbac95b9f86c4409bcb647aafbd
Maritza <maanorte@ucsc.edu>
2024-11-26 15:31:21 -0800

    s

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -2,31 +2,30 @@
 // Author: Maritza Ortega
 // Date:31 October 2024
 
-var array = [1, 2, 3, 4, 5];
+
+var array = [1, 2, 3, 4, 5]; // Original array of numbers
 
 
 function square(x) {
-    var results = x * x;
-    return results;
+    return x * x;
 }
 
 
+console.log("Testing square function:");
 console.log(square(2)); // 4
 console.log(square(3)); // 9
 console.log(square(4)); // 16
 
 
 var squaredArray = array.map(square);
-console.log("Squared Array: ", squaredArray); // [1, 4, 9, 16, 25]
+console.log("Squared Array:", squaredArray); // [1, 4, 9, 16, 25]
 
 
-var doubledArray = array.map(function(x) {
-    var results = x * 2;
-    return results;
+var doubledArray = array.map(function (x) {
+    return x * 2;
 });
-console.log("Doubled Array: ", doubledArray); // [2, 4, 6, 8, 10]
+console.log("Doubled Array:", doubledArray); // [2, 4, 6, 8, 10]
 
 
 var mapResults = array.map(square);
-console.log("Results: ", mapResults); // [1, 4, 9, 16, 25]
-
+console.log("Map Results (Squares):", mapResults); // [1, 4, 9, 16, 25]
