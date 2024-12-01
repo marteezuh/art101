8db768055cb5e10a7d11e3a3cca8af85dc697028
Maritza <maanorte@ucsc.edu>
2024-12-01 13:09:53 -0800

    k

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -4,32 +4,27 @@
 
 
 
-var array = [1, 12, 24, 48, 25];
+
+var array = [1, 4, 9, 16, 25];
 
 
 function squareRoot(x) {
-    var results = Math.sqrt(x);
-    return results;
+    return Math.sqrt(x);
+}
+
+var sqrtResults = array.map(squareRoot);
+
+
+function displayResults() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = "<h3>Square Root Results:</h3><ul>";
+    
+    sqrtResults.forEach(function(result) {
+        outputDiv.innerHTML += `<li>${result}</li>`;
+    });
+
+    outputDiv.innerHTML += "</ul>";
 }
 
 
-console.log("Test squareRoot function:");
-console.log(squareRoot(4));  // Output: 2
-console.log(squareRoot(16)); // Output: 4
-console.log(squareRoot(25)); // Output: 5
-
-
-var sqrtResults = array.map(squareRoot);
-console.log("Square Root Results:", sqrtResults); 
-
-
-var squaredResults = array.map(function(x) {
-    var results = x * x;
-    return results;
-});
-
-console.log("Squared Results:", squaredResults); 
-
-
-var mapResults = array.map(squareRoot);
-console.log("Results from squareRoot function with map():", mapResults); 
+window.onload = displayResults;
